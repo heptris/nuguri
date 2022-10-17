532b79a69021729235fc035779e97bf0c9bd23be
박상태 <sangtae365@naver.com>
2022-10-17 14:46:43 +0900

    :sparkles: [FEAT] Create project
    Create Project

---
+++ backend/nuguri/src/main/java/com/ssafy/nuguri/NuguriApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.nuguri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuguriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuguriApplication.class, args);
+	}
+
+}
