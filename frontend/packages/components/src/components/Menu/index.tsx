ebf8bdb3ecfec848e1642ec7ce5d481e12eb823f
신은정 <ejshin0213@naver.com>
2022-10-27 20:43:09 +0900

    :lipstick: [DESIGN] Menu 컴포넌트 디자인 수정

--- frontend/packages/components/src/components/Menu/index.tsx
+++ frontend/packages/components/src/components/Menu/index.tsx
@@ -4,8 +4,13 @@ import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 
 import { MenuProps, default as MuiMenu } from "@mui/material/Menu";
+import Text from "../Text";
+import { css } from "@emotion/react";
+import { racconsThemes } from "../../styles/theme";
+import styled from "@emotion/styled";
 
 export const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
+  const theme = racconsThemes.darkTheme;
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -16,8 +21,26 @@ export const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
   };
   return (
     <div>
-      <Button id="basic-button" aria-controls={open ? "basic-menu" : undefined} aria-haspopup="true" aria-expanded={open ? "true" : undefined} onClick={handleClick}>
-        Dashboard
+      <Button
+        css={css`
+          background-color: ${theme.color.background.main};
+          &:hover {
+            background-color: ${theme.color.background.main};
+          }
+        `}
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <Text
+          css={css`
+            color: ${theme.color.background.submain};
+          `}
+        >
+          취미선택
+        </Text>
       </Button>
       <MuiMenu
         {...props}
@@ -30,10 +53,32 @@ export const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
           "aria-labelledby": "basic-button",
         }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MunuItemWrapper onClick={handleClose}>
+          <Text>문화, 예술</Text>
+        </MunuItemWrapper>
+        <MunuItemWrapper onClick={handleClose}>
+          <Text>운동, 액티비티</Text>
+        </MunuItemWrapper>
+        <MunuItemWrapper onClick={handleClose}>
+          <Text>푸드, 드링크</Text>
+        </MunuItemWrapper>
+        <MunuItemWrapper onClick={handleClose}>
+          <Text>여행, 나들이</Text>
+        </MunuItemWrapper>
+        <MunuItemWrapper onClick={handleClose}>
+          <Text>창작</Text>
+        </MunuItemWrapper>
+        <MunuItemWrapper onClick={handleClose}>
+          <Text>성장, 자기계발</Text>
+        </MunuItemWrapper>
       </MuiMenu>
     </div>
   );
 });
+
+const theme = racconsThemes.darkTheme;
+const MunuItemWrapper = styled(MenuItem)`
+  &:hover {
+    background-color: ${theme.color.status.disabled};
+  }
+`;
