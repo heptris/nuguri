c91a5ce6b1c5c6023d02d775f9eb6122b399adae
qwerty1434 <qwerty1434@naver.com>
2022-11-01 17:55:39 +0900

    :pencil2: [FIX] 단위테스트가 아닌 전체테스트가 실패하는 문제 해결
    테이블을 drop하는 sql문을 클래스단위에서 실행, 클래스단위의 Sql어노테이션은 모든 메서드실행 전 해당sql이 실행됨

---
+++ backend/nuguri/src/main/resources/tableInit.sql
@@ -0,0 +1,45 @@
+DROP TABLE hobby_favorite;
+create table if not exists hobby_favorite(
+                                hobby_favorite_id bigint not null auto_increment,
+                                is_favorite bit not null,
+                                hobby_id bigint,
+                                member_id bigint,
+                                primary key (hobby_favorite_id)
+) engine=InnoDB;
+
+DROP TABLE hobby_history;
+create table if not exists hobby_history(
+    hobby_history_id bigint not null auto_increment,
+    created_date datetime(6),
+    last_modified_date datetime(6),
+    approve_status varchar(255),
+    is_promoter bit not null,
+    hobby_id bigint,
+    member_id bigint,
+    primary key (hobby_history_id)
+    ) engine=InnoDB;
+
+
+
+DROP TABLE hobby;
+create table if not exists hobby (
+                       hobby_id bigint not null auto_increment,
+                       created_date datetime(6),
+                       last_modified_date datetime(6),
+                       age_limit integer not null,
+                       content varchar(255),
+                       cur_num integer not null,
+                       end_date datetime(6),
+                       fee integer not null,
+                       hobby_image varchar(255),
+                       is_closed bit not null,
+                       max_num integer not null,
+                       meeting_place varchar(255),
+                       sex_limit char(1) not null,
+                       title varchar(255),
+                       local_id bigint,
+                       category_id bigint,
+                       primary key (hobby_id)
+) engine=InnoDB;
+
+
