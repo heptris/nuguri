e872f55f4a3ce6252939b61b328f602cfc8576ae
HojinAn <asdv94@naver.com>
2022-11-03 15:10:59 +0900

    :package: [UPDATE] component 패키지 export 추가

--- frontend/packages/components/src/components/index.tsx
+++ frontend/packages/components/src/components/index.tsx
@@ -1,2 +1,15 @@
 export { Button } from "./Button";
+export { List } from "./List";
+export { ListItem } from "./List/ListItem";
+export { Avatar } from "./Avatar";
+export { Alert } from "./Alert";
+export { Badge } from "./Badge";
+export { AppBar } from "./AppBar";
+export { BottomNavbar } from "./BottomNavbar";
+export { BottomNavbarItem } from "./BottomNavbar/BottomNavbarItem";
+export { Icon } from "./Icon";
+export { default as Text } from "./Text";
+export { LabelInput } from "./LabelInput";
+
 export { GlobalStyle } from "../styles/GlobalStyle";
+export { ThemeProvider, theme } from "../styles/theme";
