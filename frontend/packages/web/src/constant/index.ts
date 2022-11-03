c5b0599df4aaaf3e6a27b36891eb57598b8baa21
HojinAn <asdv94@naver.com>
2022-11-03 16:06:51 +0900

    :sparkles: [ADD] 라우터 pathname 상수 추가

---
+++ frontend/packages/web/src/constant/index.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/auth/login",
+  SIGNUP: "/auth/signup",
+  CHAT: "/chat",
+  LOUNGE: "/lounge",
+  NOTIFICATION: "/notification",
+  PROFILE: "/profile",
+  REGION: "/region",
+  SEARCH: "/search",
+  LOCATION: "/location",
+};
