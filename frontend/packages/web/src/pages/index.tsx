763d9212fd50dfe8bf4c01c4f8dd2e944f7472c4
SSAFY <ejshin0213@naver.com>
2022-11-04 18:01:45 +0900

    :recycle: [REFACTOR] home page에 menu와 Card 컴포넌트 연동 및 Card 스토리 수정

--- frontend/packages/web/src/pages/index.tsx
+++ frontend/packages/web/src/pages/index.tsx
@@ -1,12 +1,28 @@
 import { headerState } from "@/store";
+import * as React from "react";
+import { Card, Menu } from "@common/components";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 
 const HomePage = () => {
   const [, setHeader] = useRecoilState(headerState);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (selectedMenu: HTMLElement) => {
+    setAnchorEl(selectedMenu);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   useEffect(() => {
     setHeader({ mode: "MAIN", headingText: undefined });
-  }, []);
-  return <div>Home</div>;
+  }, [anchorEl]);
+  const newDate = new Date("2022-10-15 15:00:37");
+  return (
+    <div>
+      <Menu open={open} anchorEl={anchorEl} onCloseHandler={handleClose} onClickHandler={handleClick} />
+      <Card promiseDate={newDate} nowPeople={2} maxPeople={5} />
+    </div>
+  );
 };
 export default HomePage;
