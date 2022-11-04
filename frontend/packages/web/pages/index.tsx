6d8d1e58637a599a26ba8edc2912c4e61afe4880
신은정 <ejshin0213@naver.com>
2022-11-04 09:26:54 +0900

    :recycle: [ADD] home page에 HobbyCard 추가

--- frontend/packages/web/pages/index.tsx
+++ frontend/packages/web/pages/index.tsx
@@ -1,15 +1,17 @@
 import { css } from "@emotion/react";
 import { Animated, Basic, bounce, Combined, Pink, BasicExtended, ComponentSelectorsExtended } from "../shared/styles";
+import styled from "@emotion/styled";
 import { AppBar } from "../../components/src/components/AppBar";
 import { BottomNavbar } from "../../components/src/components/BottomNavbar";
 import { Menu } from "../../components/src/components/Menu";
+import { HobbyCard } from "@common/components/src/components/Card/HobbyCard";
 
 import Box from "@mui/material/Box";
 const Home = () => {
   return (
     <Box>
       <AppBar />
-      <Box
+      <CardWrapper
         css={css`
           display: flex;
           height: 100rem;
@@ -18,9 +20,17 @@ const Home = () => {
         `}
       >
         <Menu open={false} />
-      </Box>
+        <HobbyCard />
+      </CardWrapper>
       <BottomNavbar />
     </Box>
   );
 };
 export default Home;
+
+const CardWrapper = styled.div`
+  display: flex;
+  position: relative;
+  padding: 6rem 2rem;
+  flex-direction: column;
+`;
