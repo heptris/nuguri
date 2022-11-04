821389c562614f391672841c8a0d61dc9e30d629
SSAFY <ejshin0213@naver.com>
2022-11-04 18:00:03 +0900

    :truck: [RENAME] Card 컴포넌트 이름 변경 후 코드 수정

--- frontend/packages/components/src/components/Card/index.tsx
+++ frontend/packages/components/src/components/Card/index.tsx
@@ -21,15 +21,49 @@ import { Button } from "@mui/material";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import Text from "../Text";
+import { racconsThemes } from "../../styles/theme";
 
-export const HobbyCard = forwardRef<HTMLDivElement, CardProps>((props, ref) => {
+export type AllCardProps = CardProps & {
+  promiseDate?: Date;
+  maxPeople?: number;
+  nowPeople?: number;
+  price?: number;
+};
+
+export const Card = forwardRef<HTMLDivElement, AllCardProps>((props, ref) => {
+  const theme = racconsThemes.defaultTheme;
+  const promiseDate = props.promiseDate;
+  const week = new Array("일", "월", "화", "수", "목", "금", "토");
+  const DayOfWeek = week[promiseDate.getDay()];
+  const Month = promiseDate.getMonth() + 1;
+  const Day = promiseDate.getDate();
+  const hours = promiseDate.getHours();
+  let hour;
+  if (hours > 12) hour = "오후 " + (hours - 12);
+  else if (hours === 12) hour = "오후 " + hours;
+  else if (hours === 24) hour = "오전 " + (hours - 12);
+  else hour = "오전 " + hours;
+
   return (
     <MuiCard {...props} ref={ref} sx={{ maxWidth: 345, maxHeight: 500 }}>
       <CardMedia component="img" height="194" image="/assets/hobby/coding.jpg" alt="Coding" />
       <CardContent>
-        <Button variant="contained" color="inherit" size="small" disabled>
-          성장,자기계발
-        </Button>
+        <span
+          css={css`
+            background-color: ${theme.color.background.card};
+            border: 1px solid ${theme.color.text.hover};
+            border-radius: 10px;
+          `}
+        >
+          <Text
+            css={css`
+              color: ${theme.color.background.submain};
+              padding: 0.5rem;
+            `}
+          >
+            성장,자기계발
+          </Text>
+        </span>
         <Text
           as="h1"
           css={css`
@@ -47,10 +81,10 @@ export const HobbyCard = forwardRef<HTMLDivElement, CardProps>((props, ref) => {
               margin-right: 1rem;
             `}
           >
-            10.15(토) 오후 2시
+            {Month}.{Day}({DayOfWeek}) {hour}시
           </Text>
           <PeopleAltIcon color="disabled" />
-          <Text as="span">2/5</Text>
+          <Text as="span">{props.nowPeople + "/" + props.maxPeople}</Text>
         </DateDiv>
       </CardContent>
       <CardActions
