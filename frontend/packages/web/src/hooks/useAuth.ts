f7b4f1f0560ca1a44dd088b485cfed8605621af5
HojinAn <asdv94@naver.com>
2022-11-07 14:53:53 +0900

    :alien: [UPDATE] 쿠키로 내려 받은 accessToken에 따라 로그인 상태 결정

--- frontend/packages/web/src/hooks/useAuth.ts
+++ frontend/packages/web/src/hooks/useAuth.ts
@@ -1,17 +1,27 @@
+import { useEffect } from "react";
+import { ROUTES } from "./../constant/index";
+import { useRouter } from "next/router";
 import { useMutation } from "@tanstack/react-query";
-import { ENDPOINT_BFF } from "@/api";
+import { ACCESS_TOKEN, ENDPOINT_BFF } from "@/api";
 import { LoginFormType } from "@/types";
 import axios from "axios";
 import { atom, useRecoilState } from "recoil";
+import { getCookie } from "cookies-next";
 
 type AuthType = { isLogined: boolean };
 const authState = atom<AuthType>({
   key: "authState",
   default: { isLogined: false },
 });
+const { HOME } = ROUTES;
 
 export const useAuth = () => {
   const [{ isLogined }, setAuthState] = useRecoilState(authState);
+  const { replace } = useRouter();
+  useEffect(() => {
+    setAuthState({ isLogined: !!getCookie(ACCESS_TOKEN) });
+  }, []);
+
   const postLogin = async (form: LoginFormType) => {
     return await axios.post(ENDPOINT_BFF + "/login", form).then(({ data }) => {
       return data;
@@ -24,6 +34,7 @@ export const useAuth = () => {
   } = useMutation(postLogin, {
     onSuccess: () => {
       setAuthState({ isLogined: true });
+      replace(HOME);
     },
   });
   return { isLogined, handleLogin, isLoginError, isLoginLoading };
