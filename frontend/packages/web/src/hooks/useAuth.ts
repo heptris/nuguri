0aa1bdc1c65c406d17bd25ed83cff60019a15b3f
HojinAn <asdv94@naver.com>
2022-11-07 18:03:39 +0900

    :bug: [FIX] 새로고침시 로그아웃 문제 해결 useAuth

--- frontend/packages/web/src/hooks/useAuth.ts
+++ frontend/packages/web/src/hooks/useAuth.ts
@@ -2,16 +2,16 @@ import { useEffect } from "react";
 import { ROUTES } from "./../constant/index";
 import { useRouter } from "next/router";
 import { useMutation } from "@tanstack/react-query";
-import { ACCESS_TOKEN, ENDPOINT_BFF } from "@/api";
+import { ACCESS_TOKEN, ENDPOINT_BFF, REFRESH_TOKEN } from "@/api";
 import { LoginFormType } from "@/types";
 import axios from "axios";
 import { atom, useRecoilState } from "recoil";
-import { getCookie } from "cookies-next";
+import { deleteCookie, getCookie } from "cookies-next";
 
 type AuthType = { isLogined: boolean };
 const authState = atom<AuthType>({
   key: "authState",
-  default: { isLogined: false },
+  default: { isLogined: null },
 });
 const { HOME } = ROUTES;
 
@@ -27,15 +27,35 @@ export const useAuth = () => {
       return data;
     });
   };
+
+  const handleSilentRefresh = () => {
+    axios.get(ENDPOINT_BFF + "/login").then(data => {
+      console.log(data);
+      handleLoginProcess(data.data.accessTokenExpiresIn);
+    });
+  };
+
+  const handleLoginProcess = (expiresIn: number) => {
+    setAuthState({ isLogined: true });
+    setTimeout(handleSilentRefresh, expiresIn - Date.now());
+  };
+
   const {
     mutate: handleLogin,
     isError: isLoginError,
     isLoading: isLoginLoading,
   } = useMutation(postLogin, {
-    onSuccess: () => {
-      setAuthState({ isLogined: true });
+    onSuccess: data => {
+      handleLoginProcess(data.accessTokenExpiresIn);
       replace(HOME);
     },
   });
-  return { isLogined, handleLogin, isLoginError, isLoginLoading };
+
+  const handleLogout = () => {
+    setAuthState({ isLogined: false });
+    deleteCookie(ACCESS_TOKEN);
+    deleteCookie(REFRESH_TOKEN);
+  };
+
+  return { isLogined, handleLogin, isLoginError, isLoginLoading, handleLogout };
 };
