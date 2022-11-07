6b57f4ab543315032cbbd9aa9eb8c7e4ba1e9223
HojinAn <asdv94@naver.com>
2022-11-07 14:44:31 +0900

    :sparkles: [FEAT] ReactQuery 전역 Provider 세팅

--- frontend/packages/web/src/pages/_app.tsx
+++ frontend/packages/web/src/pages/_app.tsx
@@ -3,19 +3,23 @@ import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { RecoilRoot } from "recoil";
 import { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Layout from "@/components/Layout";
 
 const cache = createCache({ key: "next" });
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => (
   <CacheProvider value={cache}>
     <ThemeProvider theme={theme}>
       <RecoilRoot>
-        <GlobalStyle />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </QueryClientProvider>
       </RecoilRoot>
     </ThemeProvider>
   </CacheProvider>
