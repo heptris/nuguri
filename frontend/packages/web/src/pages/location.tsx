db5b92983dc3e6a651228785f09edea87d027b79
HojinAn <asdv94@naver.com>
2022-11-07 14:56:57 +0900

    :necktie: [UPDATE] useAlert, useSearchBar 적용

--- frontend/packages/web/src/pages/location.tsx
+++ frontend/packages/web/src/pages/location.tsx
@@ -1,18 +1,9 @@
-import { useHeader } from "@/hooks";
-import { searchBarState } from "@/store";
-import { useEffect } from "react";
-import { useRecoilState } from "recoil";
+import { useSearchBar, useHeader } from "@/hooks";
 
 const LocationPage = () => {
   useHeader({ mode: "SEARCH", headingText: undefined });
+  const { searchedValue } = useSearchBar("내 동네 이름(동, 읍, 면)으로 검색");
 
-  const [{ value }, setSearchBar] = useRecoilState(searchBarState);
-  useEffect(() => {
-    setSearchBar({ value, placeholder: "내 동네 이름(동, 읍, 면)으로 검색" });
-    return () => {
-      setSearchBar({ value: "", placeholder: "" });
-    };
-  }, []);
   return <div>LocationPage</div>;
 };
 
--- frontend/packages/web/src/pages/search/index.tsx
+++ frontend/packages/web/src/pages/search/index.tsx
@@ -1,17 +1,9 @@
-import { useHeader } from "@/hooks/useHeader";
-import { headerState, searchBarState } from "@/store";
-import React, { useEffect } from "react";
-import { useRecoilState } from "recoil";
+import { useHeader, useSearchBar } from "@/hooks";
 
 const SearchPage = () => {
   useHeader({ mode: "SEARCH", headingText: undefined });
-  const [{ value }, setSearchBar] = useRecoilState(searchBarState);
-  useEffect(() => {
-    setSearchBar({ value, placeholder: "취미, 중고, 공구 관련 키워드 검색" });
-    return () => {
-      setSearchBar({ value: "", placeholder: "" });
-    };
-  }, []);
+  const { searchedValue } = useSearchBar("취미, 중고, 공구 관련 키워드 검색");
+
   return <div>SearchPage</div>;
 };
 
