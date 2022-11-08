b2eae78c467388134124658bd481aa18d3ac4c5a
HojinAn <asdv94@naver.com>
2022-11-08 17:54:14 +0900

    :construction: [ING] location 페이지 개발 중

--- frontend/packages/web/src/pages/location.tsx
+++ frontend/packages/web/src/pages/location.tsx
@@ -1,28 +1,37 @@
 import { useEffect } from "react";
 import { useSearchBar, useHeader, useLocation } from "@/hooks";
+import { css } from "@emotion/react";
 
 const LocationPage = () => {
   useHeader({ mode: "SEARCH", headingText: undefined });
   const { searchedValue } = useSearchBar("내 동네 이름(동, 읍, 면)으로 검색");
   const { handleSearchAddress, searchedData, isSearching, handleSelectAddress } = useLocation();
   useEffect(() => {
-    handleSearchAddress(searchedValue);
+    searchedValue && handleSearchAddress(searchedValue);
   }, [searchedValue]);
 
   if (isSearching) return <div>Loading...</div>;
 
   return (
     <>
-      <div>{searchedData?.message}</div>
-      <div>
+      <div> {!!searchedValue ? searchedData?.message : "내 동네 검색하기"}</div>
+      <>
         {searchedData?.data.map(({ baseAddress, localId }) => {
           return (
-            <div key={localId} onClick={() => handleSelectAddress(baseAddress)}>
+            <div
+              css={css`
+                cursor: pointer;
+                border: 1px black solid;
+                margin-bottom: 1rem;
+              `}
+              key={localId}
+              onClick={() => handleSelectAddress(baseAddress)}
+            >
               {baseAddress}
             </div>
           );
         })}
-      </div>
+      </>
     </>
   );
 };
