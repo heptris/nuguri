726061d01f1640a233b009cc842d7cfeafe213f8
HojinAn <asdv94@naver.com>
2022-11-09 11:11:13 +0900

    :necktie: [UPDATE] 엔터 입력으로도 로그인하도록 로직 추가

--- frontend/packages/web/src/pages/login.tsx
+++ frontend/packages/web/src/pages/login.tsx
@@ -21,6 +21,8 @@ const LoginPage = () => {
     isLoginError && handleAlertOpen("이메일 또는 비밀번호가 잘못됐습니다.", false, 5000);
   }, [isLoginError]);
 
+  const handleSubmitLogin = () => handleLogin({ email, password });
+
   return (
     <form
       css={css`
@@ -30,6 +32,7 @@ const LoginPage = () => {
         flex-direction: column;
         align-items: center;
       `}
+      onSubmit={e => e.preventDefault()}
     >
       <LabelInput
         label={"이메일"}
@@ -43,6 +46,9 @@ const LoginPage = () => {
         value={email}
         onChange={onChangeForm}
         error={isLoginError}
+        onKeyDown={e => {
+          e.key === "Enter" && handleSubmitLogin();
+        }}
       />
       <LabelInput
         label={"비밀번호"}
@@ -56,13 +62,17 @@ const LoginPage = () => {
         value={password}
         onChange={onChangeForm}
         error={isLoginError}
+        onKeyDown={e => {
+          e.key === "Enter" && handleSubmitLogin();
+        }}
       />
       <Button
         css={css`
           width: 100%;
           margin-top: 2rem;
         `}
-        onClick={() => handleLogin({ email, password })}
+        type={"button"}
+        onClick={handleSubmitLogin}
       >
         로그인
       </Button>
