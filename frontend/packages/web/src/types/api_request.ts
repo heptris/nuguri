b9613cc10950cb1b824917b0fd1ea59e471f2534
HojinAn <asdv94@naver.com>
2022-11-09 17:58:31 +0900

    :label: [ADD] 채팅방 생성용 정보 타입 추가

--- frontend/packages/web/src/types/api_request.ts
+++ frontend/packages/web/src/types/api_request.ts
@@ -2,5 +2,13 @@ type LoginFormType = {
   email: string;
   password: string;
 };
+type ChatRoomInfoType = {
+  dealId?: number;
+  hobbyId?: number;
+  isOneToOne: boolean;
+  receiverId?: number;
+  roomName?: string;
+  senderId: number;
+};
 
-export type { LoginFormType };
+export type { LoginFormType, ChatRoomInfoType };
