579aa16a83d8096aeb4b0e2912c8f723ec4d7ac7
HojinAn <asdv94@naver.com>
2022-11-09 10:49:17 +0900

    :zap: [FIX] useDebounce를 통한 지역 검색 요청 기능 최적화 적용

--- frontend/packages/web/src/pages/location.tsx
+++ frontend/packages/web/src/pages/location.tsx
@@ -1,36 +1,38 @@
 import { useEffect } from "react";
-import { useSearchBar, useHeader, useLocation } from "@/hooks";
+import { useSearchBar, useHeader, useLocation, useDebounce } from "@/hooks";
 import { css } from "@emotion/react";
 
 const LocationPage = () => {
   useHeader({ mode: "SEARCH", headingText: undefined });
   const { searchedValue } = useSearchBar("내 동네 이름(동, 읍, 면)으로 검색");
+  const debouncedSearchedValue = useDebounce(searchedValue);
   const { handleSearchAddress, searchedData, isSearching, handleSelectAddress } = useLocation();
   useEffect(() => {
-    searchedValue && handleSearchAddress(searchedValue);
-  }, [searchedValue]);
+    debouncedSearchedValue && handleSearchAddress(debouncedSearchedValue);
+  }, [debouncedSearchedValue]);
 
   if (isSearching) return <div>Loading...</div>;
 
   return (
     <>
-      <div> {!!searchedValue ? searchedData?.message : "내 동네 검색하기"}</div>
+      <div> {!!debouncedSearchedValue ? searchedData?.message : "내 동네 검색하기"}</div>
       <>
-        {searchedData?.data.map(({ baseAddress, localId }) => {
-          return (
-            <div
-              css={css`
-                cursor: pointer;
-                border: 1px black solid;
-                margin-bottom: 1rem;
-              `}
-              key={localId}
-              onClick={() => handleSelectAddress(baseAddress)}
-            >
-              {baseAddress}
-            </div>
-          );
-        })}
+        {!!debouncedSearchedValue &&
+          searchedData?.data.map(({ baseAddress, localId }) => {
+            return (
+              <div
+                css={css`
+                  cursor: pointer;
+                  border: 1px black solid;
+                  margin-bottom: 1rem;
+                `}
+                key={localId}
+                onClick={() => handleSelectAddress(baseAddress)}
+              >
+                {baseAddress}
+              </div>
+            );
+          })}
       </>
     </>
   );
