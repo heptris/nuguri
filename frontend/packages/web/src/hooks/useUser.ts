1a96ca82162c124b8eeec6f53d621a5f9327dd05
SSAFY <ejshin0213@naver.com>
2022-11-10 11:34:37 +0900

    :sparkles: [FEAT] useUser hook에 postProfile 함수 생성

--- frontend/packages/web/src/hooks/useUser.ts
+++ frontend/packages/web/src/hooks/useUser.ts
@@ -1,13 +1,21 @@
 import { apiInstance } from "./../api/index";
 import axios from "axios";
 import { ENDPOINT_API } from "@/api";
+import { useRecoilState } from "recoil";
+import { profileState, regionState } from "@/store";
 
 export const useUser = () => {
+    const [, setLocation] = useRecoilState(regionState);
+    const [, setProfile] = useRecoilState(profileState);
+    const data = {};
     const postProfile = () => {
-        axios.
-            post(ENDPOINT_API + "/member")
+        apiInstance.
+            post(ENDPOINT_API + "/member", data)
             .then(res => {
-                console.log(res)
+                const { localId, baseAddress, nickName, temperature } = res.data.data;
+                console.log(baseAddress)
+                setLocation({ localId, baseAddress })
+                setProfile({ nickName, temperature })
             })
             .catch(e => {
                 console.log(e)
