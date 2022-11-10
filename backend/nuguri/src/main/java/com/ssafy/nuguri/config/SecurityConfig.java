656485e21b47b7a2bfcd2fa645d19a9ebd675dcb
박상태 <sangtae365@naver.com>
2022-11-10 16:48:10 +0900

    :recycle: [REFACTOR] Spring Security 로그인, 비로그인 권한 설정
    비로그인 authenticated 제외

--- backend/nuguri/src/main/java/com/ssafy/nuguri/config/SecurityConfig.java
+++ backend/nuguri/src/main/java/com/ssafy/nuguri/config/SecurityConfig.java
@@ -55,8 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
                 .authorizeRequests()
-                .antMatchers("/app/member/**", "/app/deal/**", "/api/hobby/**").authenticated()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/app/deal/{dealId}/detail", "/app/deal/list", "/auth/**").permitAll()
+                .antMatchers("/app/member/**", "/app/deal/**", "/app/hobby/**").authenticated()
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .and()
