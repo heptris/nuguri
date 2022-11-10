f1e40e8ea4f424b170c23d54830f19037752d336
HojinAn <asdv94@naver.com>
2022-11-10 17:56:09 +0900

    :label: [ADD] 채팅방 관련 타입 추가

--- frontend/packages/web/src/types/api_request.ts
+++ frontend/packages/web/src/types/api_request.ts
@@ -2,13 +2,32 @@ type LoginFormType = {
   email: string;
   password: string;
 };
-type ChatRoomInfoType = {
-  dealId?: number;
-  hobbyId?: number;
-  isOneToOne: boolean;
-  receiverId?: number;
-  roomName?: string;
+
+export type ChatRoomType = "DEAL" | "HOBBY";
+type ChatRoomInfoDefaultType = { senderId: number };
+type DealChatRoomInfoType = ChatRoomInfoDefaultType & { receiverId: number };
+type HobbyChatRoomInfoType = ChatRoomInfoDefaultType & { roomName: string };
+type ChatRoomInfoFindType<T extends ChatRoomType> = T extends "DEAL" ? DealChatRoomInfoType & { dealHisotyId: number } : HobbyChatRoomInfoType & { hobbyId: number };
+type ChatRoomInfoGetHistoryType<T extends ChatRoomType> = (T extends "DEAL" ? DealChatRoomInfoType : HobbyChatRoomInfoType) & { roomId: string };
+
+type ChatMessageType = "ENTER" | "TALK" | "LEAVE";
+type ChatRoomMessageInfoType<T extends ChatMessageType> = {
+  messageType: T;
   senderId: number;
+  roomId: string;
+} & (T extends "TALK" ? { message: string } : {});
+
+type DealItemDetailType = {
+  dealId: number;
+  title: string;
+  description: string;
+  price: number;
+  hit: number;
+  dealImage: string;
+  dong: string;
+  sellerId: number;
+  deal: boolean;
+  favorite?: boolean;
 };
 
-export type { LoginFormType, ChatRoomInfoType };
+export type { LoginFormType, ChatRoomInfoFindType, ChatRoomInfoGetHistoryType, ChatRoomMessageInfoType, DealItemDetailType };
