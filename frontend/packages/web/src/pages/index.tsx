61ad9f9434dcc7f918b2cf8a575ed3ac0694dda0
SSAFY <ejshin0213@naver.com>
2022-11-11 13:06:15 +0900

    :lipstick: [DESIGN] main page 스타일 변경 및 bottomNavigation 컴포넌트 로그인 삭제

--- frontend/packages/web/src/pages/index.tsx
+++ frontend/packages/web/src/pages/index.tsx
@@ -1,18 +1,120 @@
+import Link from "@/components/Link";
+import { ROUTES } from "@/constant";
 import { Button, Text } from "@common/components";
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import Image from "next/image";
+const { LOGIN, SIGNUP } = ROUTES;
+import mainImg from "../../public/assets/main.png";
 
 const mainPage = () => {
   return (
-    <ContainerWrapper>
-      <MainWrapper>
-        <Text>너의 구역의 취미거리 너구리</Text>
-        <Text>내 구역을 설정하고 너구리를 시작해보세요.</Text>
-      </MainWrapper>
+    <>
+      <ContainerWrapper>
+        <MainWrapper>
+          <Image src={mainImg} width={120} height={120} />
+          <Text
+            css={css`
+              font-size: 1.5rem;
+              font-weight: 700;
+              margin: 2rem 1rem 1rem;
+              color: #e4bc96;
+            `}
+          >
+            <Text
+              css={css`
+                color: white;
+              `}
+            >
+              너
+            </Text>
+            의{" "}
+            <Text
+              css={css`
+                color: white;
+              `}
+            >
+              구
+            </Text>
+            역의 취미거
+            <Text
+              css={css`
+                color: white;
+              `}
+            >
+              리
+            </Text>{" "}
+          </Text>
+
+          <Text
+            css={css`
+              font-size: 1.5rem;
+              font-weight: 700;
+              margin-bottom: 1rem;
+              color: white;
+            `}
+          >
+            너구리
+          </Text>
+          <TextWrapper>
+            <Text
+              css={css`
+                color: #f2decb;
+              `}
+            >
+              내 구역을 설정하고{" "}
+            </Text>
+            <Text
+              css={css`
+                color: #f2decb;
+              `}
+            >
+              너구리를 시작해보세요.
+            </Text>
+          </TextWrapper>
+        </MainWrapper>
+      </ContainerWrapper>
       <SubmainWrapper>
-        <Button>회원가입</Button>
-        <Text>이미 계정이 있나요? 로그인</Text>
+        <div
+          css={css`
+            width: 100%;
+            display: flex;
+            justify-content: center;
+          `}
+        >
+          <Link href={SIGNUP}>
+            <Button
+              css={css`
+                background-color: #a77c4e;
+                width: 20rem;
+                margin-bottom: 1rem;
+                border-radius: 1rem;
+                &:hover {
+                  background-color: #d3bea7;
+                }
+              `}
+            >
+              <Text>회원가입</Text>
+            </Button>
+          </Link>
+        </div>
+
+        <Text>
+          이미 계정이 있나요?{" "}
+          <Link href={LOGIN}>
+            <Text
+              css={css`
+                text-decoration: none;
+                color: #5a3d1c;
+                font-weight: 500;
+              `}
+            >
+              로그인
+            </Text>
+          </Link>
+        </Text>
       </SubmainWrapper>
-    </ContainerWrapper>
+    </>
   );
 };
 export default mainPage;
@@ -20,16 +122,39 @@ export default mainPage;
 const ContainerWrapper = styled.div`
   display: flex;
   width: 100%;
+  height: 80vh;
+  position: relative;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: #5a3d1c;
 `;
 
 const MainWrapper = styled.div`
   display: flex;
+  height: 50vh;
+  position: absolute;
   flex-direction: column;
+  justify-content: cneter;
+  align-items: center;
 `;
-const SubmainWrapper = styled.div`
+
+const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+const SubmainWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 25vh;
+  position: absolute;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  bottom: 0rem;
 `;
