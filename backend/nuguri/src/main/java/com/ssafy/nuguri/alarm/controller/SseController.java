dcc10a846c0401af262185c4c67581cefbc11fcc
전한울 <wjsgksdnf11@naver.com>
2022-11-14 09:13:52 +0900

    :sparkles: [FEAT] AlarmController, ChatRoomController Swagger 적용
    [FEAT] AlarmController, ChatRoomController Swagger 적용

--- backend/nuguri/src/main/java/com/ssafy/nuguri/alarm/controller/SseController.java
+++ backend/nuguri/src/main/java/com/ssafy/nuguri/alarm/controller/SseController.java
@@ -1,12 +1,14 @@
 package com.ssafy.nuguri.alarm.controller;
 
 import com.ssafy.nuguri.alarm.service.AlarmService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@CrossOrigin
+@Api(tags = {"Alarm 구독"})
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/app/sse")
@@ -15,6 +17,7 @@ public class SseController {
 
     private final AlarmService alarmService;
 
+    @ApiOperation(value = "알람 구독", notes = "알람 구독 API")
     @GetMapping(value = "/sub/{memberId}", produces = "text/event-stream")
     public SseEmitter subscribe(@PathVariable Long memberId) {
         return alarmService.subscribe(memberId);
