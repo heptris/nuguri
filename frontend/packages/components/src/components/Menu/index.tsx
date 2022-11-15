753999c697530db60f3a6bb13852e2211f0cf65e
SSAFY <ejshin0213@naver.com>
2022-11-15 16:46:23 +0900

    :fire: [REMOVE] Menu 컴포넌트 불필요한 코드 삭제

--- frontend/packages/components/src/components/Menu/index.tsx
+++ frontend/packages/components/src/components/Menu/index.tsx
@@ -32,10 +32,6 @@ export const Menu = forwardRef<HTMLDivElement, MenuButtomProps>(({ handleClickLi
     handleClickListItem(event.currentTarget);
   };
 
-  // const onHandleMenuItemClick = (event: React.MouseEvent<HTMLElement>, index: number) => {
-  //   props.handleMenuItemClick(index);
-  // };
-
   return (
     <div>
       <Button
@@ -44,15 +40,7 @@ export const Menu = forwardRef<HTMLDivElement, MenuButtomProps>(({ handleClickLi
           margin-bottom: 2rem;
         `}
       >
-        <ListItem
-          button
-          id="lock-button"
-          aria-haspopup="listbox"
-          aria-controls="lock-menu"
-          aria-label="when device is locked"
-          aria-expanded={open ? "true" : undefined}
-          onClick={onHandleListItemClick}
-        >
+        <ListItem button aria-expanded={open ? "true" : undefined} onClick={onHandleListItemClick}>
           <Text
             css={css`
               color: ${theme.color.background.submain};
@@ -86,69 +74,5 @@ export const Menu = forwardRef<HTMLDivElement, MenuButtomProps>(({ handleClickLi
         ))}
       </MuiMenu>
     </div>
-    // <div
-    //   css={css`
-    //     margin-bottom: 2rem;
-    //   `}
-    // >
-    //   <Button
-    //     css={css`
-    //       background-color: ${theme.color.background.main};
-    //       &:hover {
-    //         background-color: ${theme.color.background.main};
-    //       }
-    //     `}
-    //     id="basic-button"
-    //     aria-controls={props.open ? "basic-menu" : undefined}
-    //     aria-haspopup="true"
-    //     aria-expanded={props.open ? "true" : undefined}
-    //     onClick={onHandleClick}
-    //   >
-    //     <Text
-    //       css={css`
-    //         color: ${theme.color.background.submain};
-    //       `}
-    //     >
-    //       취미선택
-    //     </Text>
-    //   </Button>
-    //   <MuiMenu
-    //     {...props}
-    //     ref={ref}
-    //     id="basic-menu"
-    //     anchorEl={props.anchorEl}
-    //     open={props.open}
-    //     onClose={onHandleClose}
-    //     MenuListProps={{
-    //       "aria-labelledby": "basic-button",
-    //     }}
-    //   >
-    //     <MunuItemWrapper onClick={onHandleClose}>
-    //       <Text>문화, 예술</Text>
-    //     </MunuItemWrapper>
-    //     <MunuItemWrapper onClick={onHandleClose}>
-    //       <Text>운동, 액티비티</Text>
-    //     </MunuItemWrapper>
-    //     <MunuItemWrapper onClick={onHandleClose}>
-    //       <Text>푸드, 드링크</Text>
-    //     </MunuItemWrapper>
-    //     <MunuItemWrapper onClick={onHandleClose}>
-    //       <Text>여행, 나들이</Text>
-    //     </MunuItemWrapper>
-    //     <MunuItemWrapper onClick={onHandleClose}>
-    //       <Text>창작</Text>
-    //     </MunuItemWrapper>
-    //     <MunuItemWrapper onClick={onHandleClose}>
-    //       <Text>성장, 자기계발</Text>
-    //     </MunuItemWrapper>
-    //   </MuiMenu>
-    // </div>
   );
 });
-
-const theme = racconsThemes.defaultTheme;
-const MunuItemWrapper = styled(MenuItem)`
-  &:hover {
-    background-color: ${theme.color.status.disabled};
-  }
-`;
