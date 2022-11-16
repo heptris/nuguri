abcbf97070900bbc0acf0c91dc06be3737527e45
HojinAn <asdv94@naver.com>
2022-11-16 13:30:53 +0900

    :alien: [API] 웹 소켓 URL 수정

--- frontend/packages/web/src/api/endpoints.ts
+++ frontend/packages/web/src/api/endpoints.ts
@@ -7,5 +7,5 @@ const DOMAIN = PROTOCOL + HOST;
 export const ENDPOINT_API = DOMAIN + "/app";
 export const ENDPOINT_AUTH = DOMAIN + "/auth";
 export const ENDPOINT_BFF = "/api";
-export const ENDPOINT_WS = "http://k7a702.p.ssafy.io:8080/ws-connection";
-export const ENDPOINT_SSE = "http://k7a702.p.ssafy.io:8080/sse/sub";
+export const ENDPOINT_WS = "/ws-connection";
+export const ENDPOINT_SSE = "/sse/sub";
