55acb8e30a0f8cc7f864321524dc94c8e2366351
HojinAn <asdv94@naver.com>
2022-11-16 13:09:42 +0900

    :green_heart: [CHORE] API url 변경

--- frontend/packages/web/next.config.js
+++ frontend/packages/web/next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
   images: {
     domains: ["nuguri-bucket.s3.ap-northeast-2.amazonaws.com", "uniqon-bucket.s3.ap-northeast-2.amazonaws.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/app/:path*",
+        destination: "http://k7a702.p.ssafy.io:8080/app/:path*",
+      },
+      {
+        source: "/auth/:path*",
+        destination: "http://k7a702.p.ssafy.io:8080/auth/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = withTM(nextConfig);
--- frontend/packages/web/src/api/endpoints.ts
+++ frontend/packages/web/src/api/endpoints.ts
@@ -1,11 +1,11 @@
 /* 백엔드 엔드포인트 스키마 */
 
-const PROTOCOL = "http://";
-const HOST = "k7a702.p.ssafy.io:8080";
+const PROTOCOL = "https://";
+const HOST = "k7a702.p.ssafy.io";
 const DOMAIN = PROTOCOL + HOST;
 
 export const ENDPOINT_API = DOMAIN + "/app";
 export const ENDPOINT_AUTH = DOMAIN + "/auth";
 export const ENDPOINT_BFF = "/api";
-export const ENDPOINT_WS = DOMAIN + "/ws-connection";
-export const ENDPOINT_SSE = DOMAIN + "/sse/sub";
+export const ENDPOINT_WS = "http://k7a702.p.ssafy.io:8080/ws-connection";
+export const ENDPOINT_SSE = "http://k7a702.p.ssafy.io:8080/sse/sub";
