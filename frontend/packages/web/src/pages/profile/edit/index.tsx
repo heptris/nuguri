6d954bf17d9068e82e33b19ae5a59b7b5de93f74
SSAFY <ejshin0213@naver.com>
2022-11-16 10:20:53 +0900

    :lipstick: [DESIGN] profile edit page 스타일 수정

--- frontend/packages/web/src/pages/profile/edit/index.tsx
+++ frontend/packages/web/src/pages/profile/edit/index.tsx
@@ -1,11 +1,92 @@
-import { Text } from "@common/components";
+import { apiInstance, ENDPOINT_API } from "@/api";
+import Fileupload from "@/components/Fileupload";
+import { ROUTES } from "@/constant";
+import { useAlert, useHeader, useUser } from "@/hooks";
+import { Button, LabelInput, Text } from "@common/components";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const { HOME } = ROUTES;
 
 const EditPage = () => {
+  const { replace } = useRouter();
+  const { userInfo } = useUser();
+  const { nickname } = userInfo;
+  useHeader({ mode: "LOGIN", headingText: "프로필 수정" });
+  const { handleAlertOpen } = useAlert();
+
+  const [file, setFile] = useState(null);
+  const [nickName, setTitle] = useState(nickname);
+
+  const onClickEditButton = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    const data = {
+      nickName,
+    };
+
+    formData.append("hobbyCreateRequestDto", new Blob([JSON.stringify(data)], { type: "application/json" }));
+
+    await apiInstance
+      .post(`${ENDPOINT_API}/member/modify-nickname`, formData, config)
+      .then(res => {
+        console.log(res);
+        handleAlertOpen("프로필 수정이 완료되었습니다.", true, 1000);
+        replace(HOME);
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+        handleAlertOpen("프로필 수정이 실패했습니다.", false, 5000);
+        console.log(data);
+      });
+  };
+
   return (
-    <div>
-      <Text>프로필 수정 페이지</Text>
-    </div>
+    <ContainerWrapper>
+      <Fileupload onFileSelectSuccess={(file: any) => setFile(file)} onFileSelectError={({ error }) => handleAlertOpen("이미지 등록에 실패했습니다.", false, 5000)} type={"profile"} />
+      <LabelInput
+        label={"닉네임"}
+        variant={"standard"}
+        css={css`
+          width: 80%;
+          margin-top: 1rem;
+        `}
+        name={"nickName"}
+        value={nickName}
+        placeholder={"2~15 글자로 작성해주세요."}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setTitle(event.target.value);
+        }}
+      />
+      <Button
+        onClick={onClickEditButton}
+        css={css`
+          width: 80%;
+          margin-top: 2rem;
+        `}
+      >
+        등록
+      </Button>
+    </ContainerWrapper>
   );
 };
 
 export default EditPage;
+
+const ContainerWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 85vh;
+  flex-direction: column;
+  align-items: center;
+`;
