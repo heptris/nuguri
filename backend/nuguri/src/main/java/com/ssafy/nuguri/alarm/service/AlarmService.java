bcd06a0c6ef999f89d911575904c7e33e99649eb
전한울 <wjsgksdnf11@naver.com>
2022-11-18 17:21:01 +0900

    :sparkles: [FEAT] dummy data 추가
    [FEAT] dummy data 추가

--- backend/nuguri/src/main/java/com/ssafy/nuguri/alarm/service/AlarmService.java
+++ backend/nuguri/src/main/java/com/ssafy/nuguri/alarm/service/AlarmService.java
@@ -10,6 +10,7 @@ import com.ssafy.nuguri.exception.ex.ErrorCode;
 import com.ssafy.nuguri.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
